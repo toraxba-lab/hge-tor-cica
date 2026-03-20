5d0739173994adcd20dab194cecf20283e2a1825
toraxba-lab <toraxba@gmail.com>
2026-03-20 14:24:45 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,41 +1,15 @@
-import type {NextConfig} from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
   eslint: {
+    // Isso evita que erros de formatação barrem o deploy
     ignoreDuringBuilds: true,
   },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-    ],
+  typescript: {
+    // Isso evita que erros de tipagem barrem o deploy (útil para finalizar logo)
+    ignoreBuildErrors: true,
   },
-  experimental: {
-    allowedDevOrigins: [
-      '*.cloudworkstations.dev',
-      'localhost:9002'
-    ]
-  }
 };
 
 export default nextConfig;
