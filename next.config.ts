568842d11b5e50019bf85061cc233ad1cee9347d
toraxba-lab <toraxba@gmail.com>
2026-03-20 14:27:08 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* Configurações básicas para o deploy fluir */
   eslint: {
-    // Isso evita que erros de formatação barrem o deploy
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // Isso evita que erros de tipagem barrem o deploy (útil para finalizar logo)
     ignoreBuildErrors: true,
   },
+  // Removido o bloco "experimental" que estava causando o erro
 };
 
 export default nextConfig;
